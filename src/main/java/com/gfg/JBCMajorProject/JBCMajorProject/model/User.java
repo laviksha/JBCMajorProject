37a64cb423d66c0bc8fcc4d4a81f95fbe9e238e1
laviksha <lavikshasoni@gmail.com>
2019-06-29 09:59:47 +0530

    add id in model User

--- src/main/java/com/gfg/JBCMajorProject/JBCMajorProject/model/User.java
+++ src/main/java/com/gfg/JBCMajorProject/JBCMajorProject/model/User.java
@@ -1,6 +1,15 @@
 package com.gfg.JBCMajorProject.JBCMajorProject.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  long id;
     private String handle;
     private int contribution;
     private int lastOnlineTimeSeconds;
@@ -20,6 +29,14 @@ public class User {
         this.avatar = avatar;
         this.titlePhoto = titlePhoto;
         this.registrationTimeSeconds = registrationTimeSeconds;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getHandle() {
---
+++ src/main/java/com/gfg/JBCMajorProject/JBCMajorProject/service/UserDao.java
@@ -0,0 +1,4 @@
+package com.gfg.JBCMajorProject.JBCMajorProject.service;
+
+public class UserDao {
+}
